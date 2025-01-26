cdbd67df78f564081df61dfd7f878f04d1c219f1
PrathamSaroch <prathamsaroch9@gmail.com>
2025-01-26 16:37:19 +0530

    Cities Shows in Front Page. And Backend of same. (User Side)

--- src/main/java/Salonique/SaloonManagement/Connection/Database.java
+++ src/main/java/Salonique/SaloonManagement/Connection/Database.java
@@ -27,4 +27,3 @@ public class Database
         return rs;
     }
 }
-
--- src/main/java/Salonique/SaloonManagement/Controllers/UserRestController.java
+++ src/main/java/Salonique/SaloonManagement/Controllers/UserRestController.java
@@ -1,8 +1,23 @@
 package Salonique.SaloonManagement.Controllers;
 
+import Salonique.SaloonManagement.Connection.Database;
+import Salonique.SaloonManagement.Connection.RDBMS_TO_JSON;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserRestController {
-    
+
+    @GetMapping("/userShowCities")
+    public String userShowCities() {
+        try {
+            String ans = new RDBMS_TO_JSON().generateJSON("select * from cities");
+
+            System.out.println(ans);
+
+            return ans;
+        } catch (Exception ex) {
+            return ex.toString();
+        }
+    }
 }
