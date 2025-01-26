b56e053c2779bd2fc2221c3ee2e2a05505f99e20
PrathamSaroch <prathamsaroch9@gmail.com>
2025-01-26 15:10:05 +0530

    have added a page in the templates folder and opened it using controllers

--- src/main/java/Salonique/SaloonManagement/Controllers/UserController.java
+++ src/main/java/Salonique/SaloonManagement/Controllers/UserController.java
@@ -1,8 +1,15 @@
 package Salonique.SaloonManagement.Controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserController {
     
+    @GetMapping("/")
+    public String index()
+    {
+        System.out.println("In Controller");
+        return "index-3";
+    }
 }
