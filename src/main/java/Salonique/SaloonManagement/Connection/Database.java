a946fcc0920277382d4f5eaa2024c79237e662d2
PrathamSaroch <prathamsaroch9@gmail.com>
2025-02-08 20:39:07 +0530

    Owner Manage Bookings

--- src/main/java/Salonique/SaloonManagement/Connection/Database.java
+++ src/main/java/Salonique/SaloonManagement/Connection/Database.java
@@ -12,7 +12,7 @@ public class Database
             Class.forName("com.mysql.cj.jdbc.Driver");
             System.out.println("Driver loaded successfully!!");
             //create connection to the mysql database
-            Connection conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/saloonmanagement","root", "1485");
+            Connection conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/saloonmanagement","root", "System123@");
             System.out.println("Connection build");
             Statement stmt = conn.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE,
                     ResultSet.CONCUR_UPDATABLE);
